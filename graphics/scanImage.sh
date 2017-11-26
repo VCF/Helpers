aac67c550609ea7e38da7c53062d3902ee184ea7
Charles Tilford <gitcommit@biocode.fastmail.fm>
2017-11-26 13:12:11 -0500

    flatbed scanning script

---
+++ graphics/scanImage.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+## Scans a single image from a flatbed scanner, assigns a date-stamped
+## name, stores in a folder for later editting / renaming
+
+## You'll likely need to change these two values:
+## The name of the scanner, use scanimage -L to find out what yours is:
+DEVNAME='epkowa'
+## The folder where the scanned images should be stored:
+DIR='/abyss/Common/ToFile/Scanner'
+
+
+## use scanimage -L and grep to get the full device name, including
+## the USB numbering:
+DEVICE=`scanimage -L | egrep -o "$DEVNAME:[a-z]+:[0-9]+:[0-9]+"`
+
+FILE=`date +'scannedImage_%Y-%m-%d_%H:%M:%S'`
+CMD="scanimage --progress --mode color"
+
+CMD="$CMD --format=tiff";
+# CMD="$CMD --progress";
+FILE="$FILE.tiff";
+
+FILEPATH="$DIR/$FILE"
+
+echo -e "
+Scanning document:
+  From: \033[35m$DEVICE\033[0m
+    To: \033[34m$DIR
+\033[31m"
+
+`$CMD  -d "$DEVICE" > $FILEPATH`
+
+echo -e "\033[0m
+  File: \033[36m$FILE\033[0m
+"
+
+sleep 10
