0b320bce1c0938d88100dd86d0cec0b3e7c9b1c2
Charles Tilford <gitcommit@biocode.fastmail.fm>
2026-02-07 09:23:16 -0500

    abstracing command line sound playback

---
+++ utils/playSound.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+ABOUT="
+Tracking the tools Linux uses to play sound files from the command line
+has become tedious. This basic script just centralizes the current
+utility that's working in the most recent versions of Mint
+
+Prior tools that don't work now:
+  gst123
+
+"
+
+# The executable we'll use
+psExe="play"
+# The package it resides in
+psPack="sox"
+
+# Can we locate it?
+psWhc="$(which $psExe)"
+if [ -z "$psWhc" ]; then
+    # Nope, can't find it
+        echo -e "
+[ERR] playSound uses $psExe to play the sound clip
+
+    sudo apt-get install $psPack
+"
+fi
+
+function playSound {
+    SOUND="$1" # The audio file we will play
+    OPTS="$2"  # Optional options to pass to the player
+    if [ -z "$psWhc" ]; then
+        # Tool not available
+        # We already gave feedback when we sourced this file, just return
+        return
+    elif [ -z "$SOUND" ]; then
+        echo "[ERR] playSound must be provided a path to a sound file"
+        return
+    elif [ ! -s "$SOUND" ]; then
+        echo -e "[ERR] playSound could not find the requested sound file:
+  $SOUND"
+        return
+    fi
+    PSE="$($psWhc -q "$SOUND" $OPTS 2>/dev/null)"
+}
